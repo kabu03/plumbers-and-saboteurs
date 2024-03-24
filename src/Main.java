c7230380f923986ef4bfcb8a47eea1b58cc7258e
Ibrahim Muheisen <ibrahimmuheisen@gmail.com>
2024-03-24 21:53:27 +0100

    main adjusted with welcome message and option to either start or exit game

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,25 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-            Game game=new Game();
-            game.initGame();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Pipes in the Desert CLI Game by Team MANSAF!");
+        System.out.println("Are you ready to start?");
+        System.out.println("1. Start Game");
+        System.out.println("2. Exit");
 
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+                Game game = new Game();
+                game.initGame();
+                break;
+            case 2:
+                System.out.println("Exiting the game. Goodbye!");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid choice. Exiting the game.");
+                System.exit(0);
+        }
     }
 }
-
