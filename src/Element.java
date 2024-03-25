0fa074244267a29a3d9398d8ceb95953f815d2f1
Karam Abu Judom <karam.abujudom@gmail.com>
2024-03-25 12:10:50 +0100

    Added JavaDoc comments

--- src/Element.java
+++ src/Element.java
@@ -5,30 +5,76 @@
  * connections among each other.
  */
 public abstract class Element {
+    /**
+     * A boolean that indicates whether the element is currently occupied by a player or not.
+     */
     protected boolean occupied = false;
+    /**
+     * A boolean indicating whether the element is currently in working condition or not.
+     * For example, a broken pump would have this value as False.
+     */
     protected boolean works = true;
+    /**
+     * An integer that represents the current water level in the element.
+     */
     protected int waterLevel;
+    /**
+     * An integer that represents the maximum amount of water that
+     * the element can theoretically hold.
+     */
     protected int maxCapacity;
+    /**
+     * A boolean that indicates whether the element is currently clicked.
+     */
     protected boolean isClicked = false;
+    /**
+     * Represents an array of pipes that are connected to a certain element.
+     */
     protected Pipe[] connectedPipes;
+    /**
+     * An integer that represents the maximum number of pipes that can be theoretically connected to an element.
+     */
     protected int maxConnectablePipes;
+    /**
+     * A boolean that indicates whether the element can be stood upon.
+     */
     protected boolean standable = false;
+
+    /**
+     * Checks if the element is in working condition and returns a boolean.
+     */
     public boolean isWorking(){
         System.out.println("isWorking()");
         return works;
     }
+
+    /**
+     * Checks if the element is currently occupied and returns a boolean.
+     */
     public boolean isOccupied(){
         System.out.println("isOccupied()");
         return occupied;
     }
+
+    /**
+     * Increases the water level within the element.
+     */
     public void incrementWater(){
         System.out.println("incrementWater()");
         waterLevel+=2;
     }
+
+    /**
+     * Decreases the water level within the element.
+     */
     public void decrementWater(){
         System.out.println("decrementWater()");
         waterLevel-=2;
     }
+
+    /**
+     * Sets the occupied status of an element.
+     */
     public void setOccupied(boolean bool){
         System.out.println("setOccupied(boolean)");
         occupied = bool;
--- src/EndOfPipe.java
+++ src/EndOfPipe.java
@@ -3,15 +3,29 @@
  * elements. It retains information about the connected element for later retrieval.
  */
 public class EndOfPipe {
+    /**
+     * An attribute that stores the element that is currently connected to the end of pipe object.
+     */
     private Element connectedElement;
+
+    /**
+     * A method that connects the end of pipe object to an element in the system.
+     * @param e The element that should be connected to.
+     */
     public void connectToElement(Element e){
         System.out.println("connectToElement(Element)");
 
     }
+
+    /**
+     * A method that disconnects the end of pipe object from an element in the system.
+     * @param e The element that should be disconnected from.
+     */
     public void disconnectFromElement(Element e){
         System.out.println("disconnectFromElement(Element)");
 
     }
+
     public Element getConnectedElement(){
         System.out.println("getConnectedElement()");
         return connectedElement;
