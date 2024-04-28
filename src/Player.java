7ca1f4e52e621ce01a607bc48007e8cf186e7b8d
NafezSayyad <62516699+NafezSayyad@users.noreply.github.com>
2024-04-28 13:54:13 +0200

    Update Player.java
    update the output to match the testcases

--- src/Player.java
+++ src/Player.java
@@ -26,134 +26,161 @@ public abstract class Player {
         System.out.println("Player " + playerName + " passed their turn.");
     }
 
-    /**
+       /**
      * Changes the input pipe of a specified pump to a new pipe based on user input.
-     * The method prompts the user to select a pump and then choose which of the
+     * The method checks if the user is on a pump or not
+     * The method prints all the current connected elements to the pump
      * connected pipes to set as the new input pipe. It performs checks to ensure the action
-     * is permissible and outputs the result of the change. The method assumes that there are 5 pumps in the game, and 3 connected
-     * pipes connected to every pump. In the GUI implementation, this will be different.
+     * is permissible and outputs the result of the change.
+     * some pipes are connected, and some are connectable. Method will only show connected pipes
      *
-     * @author Karam Abu Judom
-     */
+     *
+     * @author Nafez Mousa Sayyad
+     **/
     protected void changeInputPipe(Game game) {
-        if (!(currentElement instanceof Pump currentPump)) {
-            if (!Game.testMode)  System.out.println("You are not currently on a pump. Move to a pump first.");
+        if (!(currentElement instanceof Pump)) {
+            System.out.println("You are not currently on a pump. Move to a pump first.");
             return;
         }
 
-        if (!Game.testMode) System.out.println("Currently connected pipes to the pump '" + currentPump.getName() + "':");
+        Pump currentPump = (Pump) currentElement;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Currently connected pipes to the pump '" + currentPump.getName() + "':");
         List<Pipe> connectablePipes = currentPump.connectablePipes;
         for (int i = 0; i < connectablePipes.size(); i++) {
-            if (!Game.testMode)    System.out.println((i + 1) + ". " + connectablePipes.get(i).getName());
+            System.out.println((i + 1) + ". " + connectablePipes.get(i).getName());
         }
 
-        if (!Game.testMode) System.out.println("Select the number of the pipe to set as the new input pipe:");
+        System.out.println("Select the number of the pipe to set as the new input pipe:");
         int pipeNumber;
         try {
-            pipeNumber = Integer.parseInt(Game.scanner.nextLine());
+            pipeNumber = sc.nextInt();
             if (pipeNumber < 1 || pipeNumber > connectablePipes.size()) {
-                if (!Game.testMode)    System.out.println("Invalid input. Select a number listed above.");
+                System.out.println("Invalid input. Select a number listed above.");
             } else {
                 Pipe selectedPipe = connectablePipes.get(pipeNumber - 1);
                 currentPump.inPipe = selectedPipe; // Setting the selected pipe as new input pipe
-                System.out.println("New input pipe set to: " + selectedPipe.getName());
+                System.out.println(playerName+" changed the input pipe of "+currentPump.getName()+" to " + selectedPipe.getName());
             }
         } catch (InputMismatchException e) {
-            if (!Game.testMode)   System.out.println("Invalid input. Please enter a number.");
+            System.out.println("Invalid input. Please enter a number.");
         }
     }
 
 
     /**
-     * Changes the output pipe of a specified pump to a new pipe based on user input.
-     * Similar to {@link #changeInputPipe(Game game)}, this method prompts the user
-     * to select a pump and then choose one of the connected pipes to set as the
-     * new output pipe. It checks if the action can be performed and displays the result.
-     * @author Karam Abu Judom
+     * Similar to the changeInputPipe, once the user is on a pump, he will be able
+     * to change the input of that specific pump by calling this method
+     * the method, check if the user is on a pump or not
+     * then prints the connected elements to that current pump the user is standing on
+     * the user can choose which pipe to become the input for the pump
+     * @author Nafez Mousa Sayyad
      */
     protected void changeOutputPipe(Game game) {
-        if (!(currentElement instanceof Pump currentPump)) {
-            if (!Game.testMode)  System.out.println("You are not currently on a pump. Move to a pump first.");
+        if (!(currentElement instanceof Pump)) {
+            System.out.println("You are not currently on a pump. Move to a pump first.");
             return;
         }
 
-        if (!Game.testMode) System.out.println("Currently connected pipes to the pump '" + currentPump.getName() + "':");
+        Pump currentPump = (Pump) currentElement;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Currently connected pipes to the pump '" + currentPump.getName() + "':");
         List<Pipe> connectablePipes = currentPump.connectablePipes;
         for (int i = 0; i < connectablePipes.size(); i++) {
-            if (!Game.testMode) System.out.println((i + 1) + ". " + connectablePipes.get(i).getName());
+            System.out.println((i + 1) + ". " + connectablePipes.get(i).getName());
         }
 
-        if (!Game.testMode) System.out.println("Select the number of the pipe to set as the new output pipe:");
+        System.out.println("Select the number of the pipe to set as the new output pipe:");
         int pipeNumber;
         try {
-            pipeNumber = Integer.parseInt(Game.scanner.nextLine());
+            pipeNumber = sc.nextInt();
             if (pipeNumber < 1 || pipeNumber > connectablePipes.size()) {
-                if (!Game.testMode)    System.out.println("Invalid input. Select a number listed above.");
+                System.out.println("Invalid input. Select a number listed above.");
             } else {
                 Pipe selectedPipe = connectablePipes.get(pipeNumber - 1);
                 currentPump.outPipe = selectedPipe; // Setting the selected pipe as new output pipe
-                System.out.println("New output pipe set to: " + selectedPipe.getName());
+                System.out.println(playerName + " changed the output pipe of " + currentPump.getName()+" to " + selectedPipe.getName());
             }
         } catch (InputMismatchException e) {
-            if (!Game.testMode)  System.out.println("Invalid input. Please enter a number.");
+            System.out.println("Invalid input. Please enter a number.");
         }
     }
 
 
     /**
-     * Facilitates player movement between different game locations, including pipes, pumps, and a cistern.
-     * This method engages the player with a choice-driven interface to select their next destination within the game's environment.
-     * Upon selection, the method evaluates the feasibility of the move based on game logic, such as the occupancy state of the target location.
+     * The move method, is called once a player wants to move his location
+     * ( move to a new element)
+     * once the user chooses to move, he will be able to select any of the current standable element in the
+     * elementList, (every element in the game)
+     * if a user moves to a pipe, the method sets this pipe as occupied.
+     * the other players are able to see that the pipe is occupied.
+     * if in test mode, the method is able to accept strings as input to move to a location
      *
-     * The method operates in several steps:
-     * 1. Prompts the user to choose their desired destination type (pipe, pump, or cistern) with an input choice.
-     * 2. Based on the selection, further prompts guide the user to specify their destination more precisely (e.g., which pipe or pump).
-     * 3. Checks for conditions that might prevent the move, such as an occupied destination.
-     * 4. Updates the player's location in the game world if the move is valid.
-     * @author : Majed
+     * @author Nafez Mousa Sayyad
      */
     protected void move(Game game) {
-        int userChoice;
+        Scanner sc = new Scanner(System.in);
+        String userInput;
+        Element chosenElement = null;
 
         // Displaying all current elements in the game
-        if(!Game.testMode) System.out.println("Please select the number of the element you want to move to:");
+
         List<Element> elements = game.elementList;
-    for (int i = 0; i < elements.size(); i++) {
-        Element element = elements.get(i);
-        // Displaying occupancy status only for Pipe elements
-        String occupancyStatus = (element instanceof Pipe && element.isOccupied()) ? " (Occupied)" : "";
-        if(!Game.testMode) System.out.println((i + 1) + ". " + element.getName() + occupancyStatus);
-    }
+
+        for (int i = 0; i < elements.size(); i++) {
+            Element element = elements.get(i);
+            String occupancyStatus = (element instanceof Pipe && element.isOccupied()) ? " (Occupied)" : "";
+            System.out.println((i + 1) + ". " + element.getName() + occupancyStatus);
+        }
 
         // Get user input
         while (true) {
             try {
-                userChoice = Integer.parseInt(Game.scanner.nextLine()) - 1; // Adjust for 0-based index
-                if (userChoice >= 0 && userChoice < elements.size()) {
-                    Element chosenElement = elements.get(userChoice);
-                    if (currentElement != chosenElement) {
-                        if (!(chosenElement instanceof Pipe) || !chosenElement.isOccupied()) { // Check if it is a pipe and not occupied
-                            if (currentElement != null && currentElement instanceof Pipe) {
-                                currentElement.setOccupied(false); // Mark current element as not occupied if it is a pipe
-                            }
-                            currentElement = chosenElement; // Move player to new element
-                            if (chosenElement instanceof Pipe) {
-                                chosenElement.setOccupied(true); // Mark new element as occupied if it is a pipe
-                            }
-                            System.out.println(playerName + " moved to " + chosenElement.getName());
-                        } else {
-                            System.out.println("This location is currently occupied. Choose another location.");
-                        }
+                if (!Game.testMode) {
+                    int userChoice = sc.nextInt() - 1; // Adjust for 0-based index
+                    if (userChoice >= 0 && userChoice < elements.size()) {
+                        chosenElement = elements.get(userChoice);
                     } else {
-                        System.out.println("You are already at this location.");
+                        System.out.println("Invalid choice. Please enter a number between 1 and " + elements.size());
+                        continue;
+                    }
+                } else {
+                    userInput = sc.nextLine().trim();
+                    for (Element element : elements) {
+                        if (element.getName().equalsIgnoreCase(userInput)) {
+                            chosenElement = element;
+                            break;
+                        }
+                    }
+                    if (chosenElement == null) {
+                        System.out.println("No element matches the name entered. Please try again.");
+                        continue;
+                    }
+                }
+
+                if (currentElement != chosenElement) {
+                    if (!(chosenElement instanceof Pipe) || !chosenElement.isOccupied()) {
+                        if (currentElement != null && currentElement instanceof Pipe) {
+                            currentElement.setOccupied(false); // Mark current element as not occupied if it is a pipe
+                        }
+                        currentElement = chosenElement; // Move player to new element
+                        if (chosenElement instanceof Pipe) {
+                            chosenElement.setOccupied(true); // Mark new element as occupied if it is a pipe
+                        }
+                        System.out.println( playerName + " moved to " + chosenElement.getName());
+                    } else {
+                        System.out.println("This location is currently occupied. Choose another location.");
                     }
                     break;
                 } else {
-                    System.out.println("Invalid choice. Please enter a number between 1 and " + elements.size());
+                    System.out.println("You are already at this location.");
+                    break;
                 }
             } catch (InputMismatchException e) {
                 System.out.println("Invalid input. Please enter a number.");
-                Game.scanner.next(); // Clear the invalid input
+                sc.next(); // Clear the invalid input
             }
         }
     }
