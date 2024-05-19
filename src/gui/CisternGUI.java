d0775edd1b4329addf667136b1441c9491a67476
NafezSayyad <62516699+NafezSayyad@users.noreply.github.com>
2024-05-19 15:52:23 +0200

    java doc CisternGUI.java

--- src/gui/CisternGUI.java
+++ src/gui/CisternGUI.java
@@ -1,11 +1,23 @@
 package gui;
+
 import javax.swing.*;
 import java.awt.*;
 import model.Cistern;
 
+/**
+ * The CisternGUI class represents a graphical user interface for displaying a cistern.
+ * It handles loading and scaling the cistern image and drawing it on a given graphics context.
+ */
 public class CisternGUI {
     private Cistern cistern;
     public ImageIcon cisternImg;
+
+    /**
+     * Constructs a CisternGUI with the specified Cistern.
+     * Loads and scales the cistern image to match the cistern's dimensions.
+     *
+     * @param cistern the Cistern object representing the cistern model
+     */
     public CisternGUI(Cistern cistern){
         this.cistern = cistern;
         Image originalImage = new ImageIcon("src\\gui\\images\\Cistern.png").getImage();
@@ -13,6 +25,11 @@ public class CisternGUI {
         this.cisternImg = new ImageIcon(scaledImage);
     }
 
+    /**
+     * Draws the cistern image at the cistern's position on the given graphics context.
+     *
+     * @param g the Graphics context on which to draw the cistern image
+     */
     public void draw(Graphics g){
         Point pos = cistern.getPosition();
         g.drawImage(cisternImg.getImage(), pos.x, pos.y, null);
